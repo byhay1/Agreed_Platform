e4231b51a27aab758f07ce601bc1cc0ff28034f4
Byron <byhay1@terpmail.umd.edu>
2022-06-06 11:23:06 -0400

    update token name/symbol

--- Agreed_Token/contracts/AgreedToken.sol
+++ Agreed_Token/contracts/AgreedToken.sol
@@ -4,7 +4,7 @@ pragma solidity >=0.5.16 <0.9.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract AgreedToken is ERC20 {
-    constructor(uint256 initialSupply) public ERC20("AgreedToken", "AGREE"){
+    constructor(uint256 initialSupply) public ERC20("Agreed", "AGRD"){
         _mint(msg.sender, initialSupply);
   }
 }
