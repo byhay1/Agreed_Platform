199ed54a3d21a1ef34882f9e0294c5301df38715
Byron <byhay1@terpmail.umd.edu>
2022-07-02 23:26:16 -0400

    added more html,base layout

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,50 @@ import logo from './logo.svg';
 
 function App() {
   return (
-    <h1>Hello</h1>
+    //--- Selection Bar,Show level, amount of AGRD,
+    <div class="container">
+      <div class="container">
+        <div class="row">
+          <div class="col">
+            <AREA FOR BUTTONS>
+          </div>
+          <div class="col-md-auto">
+            Your Tier: null //static for now
+          </div>
+          <div class="col col-lg-2">
+            Your Balance: 0 //static for now
+          </div>
+        </div>
+      </div>
+    </div>
+
+    //--- Introduction
+    <div class="container">
+        <!-- Content here -->
+        <p> Fill with some sort of website introduction once completed - or a template</p><p></p><p></p>
+    </div>
+
+    //---second part of page
+
+    <div class="container">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm">
+            <h3> Diamond </h3>
+            <p>
+          </div>
+          <div class="col-sm">
+            Platinum
+          </div>
+          <div class="col-sm">
+            Gold
+          </div>
+          <div class="col-sm">
+            Silver
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
