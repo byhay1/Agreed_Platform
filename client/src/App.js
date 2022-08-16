a699094bae9868525cfd301c042618ccd8b7116c
simon <simon.r.both@gmail.com>
2022-08-16 11:37:20 +0200

    ui implementation

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,16 @@ import { ethers } from "ethers";
 import addresses from "./addresses.js";
 import abis from "./abis.js";
 
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+
+import Button from "react-bootstrap/Button";
+
+import RankCard from "./components/RankCard.js";
+
 /* 
 TODO
 - Metamask integration ✔️
@@ -12,22 +22,23 @@ TODO
 - create three links that will send a user to another page
   that'll be an entry to interact with my second contract. 
   For now that page will have a button with the classification
-  and a "under construction" as I'm not done with the second contract. 
+  and a "under construction" as I'm not done with the second contract. ✔️
 
     - Each link corresponds to the user's(address') classification/level. 
     (ex. red class is higher than yellow and yellow is higher than green,
     thus red can access all three, yellow can access only yellow/green 
-    links and green can only access green link).
+    links and green can only access green link). ✔️
 - dead links at "learn more", "about", "contact us", "team", "(instragram icon)",
   "(telegram icon)", "(discord icon)", "(facebook icon)" - 
-  I can create these later as they will be static/href and easy to do. 
+  I can create these later as they will be static/href and easy to do. ✔️
 
+- UI as in mockup wireframe ✔️
 */
 function App() {
   const [rank, setRank] = useState();
   const [balance, setBalance] = useState();
-  const [depositValue, setDepositValue] = useState("");
-  const [rankValue, setRankValue] = useState("");
+  /* const [depositValue, setDepositValue] = useState("");
+  const [rankValue, setRankValue] = useState(""); */
   const [userAddress, setUserAddress] = useState();
 
   // A Web3Provider wraps a standard Web3 provider, which is
@@ -74,6 +85,9 @@ function App() {
 
   const UserRank = ["None", "Silver", "Gold", "Platinum", "Diamond"];
 
+  /* 
+  commented out because not needed 
+  
   const handleDeposit = (e) => {
     setDepositValue(e.target.value);
   };
@@ -90,23 +104,111 @@ function App() {
   const handleRankSubmit = (e) => {
     e.preventDefault();
     console.log(rankValue);
-  };
+  }; */
   return (
-    //--- Selection Bar,Show level, amount of AGRD
-    <Fragment>
-      <div class="container">
-        <div class="container">
-          <div class="row">
-            <div class="col">AREA FOR BUTTONS</div>
-            <div>{userAddress ? userAddress : ""}</div>
-            <div class="col-md-auto">
-              Your Tier: {rank ? UserRank[rank] : "None"}
-            </div>
-            <div class="col col-lg-2">
-              Your Balance: {balance ? balance : ""}
-            </div>
+    <Container fluid style={{ backgroundColor: "#a1fcd9", minHeight: "100vh" }}>
+      {/* Navbar  */}
+      <Row>
+        <Navbar collapseOnSelect expand="md" bg="light" variant="light">
+          <Container fluid>
+            <img src="./logo.png" width="70" height="70" alt="logo" />
+            <Navbar.Brand href="#home">
+              <h3>Contracts AGREED Upon </h3>
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
 
-            <div class="col col-lg-2">
+            <Navbar.Collapse id="responsive-navbar-nav">
+              <Nav className="container-fluid">
+                <Nav.Item className="ms-5">
+                  <h5>
+                    {userAddress
+                      ? "Wallet: " + userAddress.substring(0, 7) + "..."
+                      : ""}
+                  </h5>
+                </Nav.Item>
+                <Nav.Item className="ms-auto">
+                  <h5>{rank >= 0 ? "LVL: " + UserRank[rank] : ""}</h5>
+                </Nav.Item>
+                <Nav.Item className="ms-5">
+                  <h5>{balance ? "AGRD: " + balance : ""}</h5>
+                </Nav.Item>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </Row>
+      {/* Main Content */}
+      <Row style={{ marginTop: "10px", marginBottom: "10px" }}>
+        {/* left side */}
+        <Col xs={12} md={8}>
+          <Container>
+            {/* Cards */}
+            <Row className="flex-grow-1 justify-content-center align-items-center">
+              {true /* change to these values for real scenario:
+                     rank
+                      ? Array(rank) 
+                      
+                      for testing purpose it shows all 4 possible ranks*/
+                ? Array(4)
+                    .fill(1)
+                    .map((el, i) => {
+                      return (
+                        <RankCard
+                          key={i}
+                          rankname={UserRank[i + 1]}
+                          index={i + 1}
+                        />
+                      );
+                    })
+                : ""}
+            </Row>
+          </Container>
+        </Col>
+        {/* right side */}
+        <Col
+          xs={6}
+          md={4}
+          className="d-flex flex-column justify-content-center align-items-start"
+        >
+          <h3>
+            Learn how to convert your physical contracts into private NFTs
+          </h3>
+          <Button variant="light" size="lg" href="learn">
+            Learn More!
+          </Button>
+        </Col>
+      </Row>
+
+      {/* Footer */}
+      <Row>
+        <Navbar bg="#a1fcd9" variant="light">
+          <Container className="">
+            <Nav className="container-fluid flex-wrap">
+              <Nav.Link className="ms-auto" href="/about">
+                About
+              </Nav.Link>
+              <Nav.Link href="/contact">Contact Us</Nav.Link>
+              <Nav.Link href="/team">Team</Nav.Link>
+              <Nav.Link href="/Instagram">Instagram</Nav.Link>
+              <Nav.Link href="/Telegram">Telegram</Nav.Link>
+              <Nav.Link href="/Discord">Discord</Nav.Link>
+              <Nav.Link href="/Facebook">Facebook</Nav.Link>
+            </Nav>
+          </Container>
+        </Navbar>
+      </Row>
+    </Container>
+  );
+}
+
+export default App;
+
+{
+  /*  
+        commented out 
+        because of no use right now 
+
+        <div class="col col-lg-2">
               <form onSubmit={handleDepositSubmit}>
                 <div class="form-group">
                   <input
@@ -135,77 +237,6 @@ function App() {
                   Get Rank
                 </button>
               </form>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div class="container">
-        <p>
-          {" "}
-          Fill with some sort of website introduction once completed - or a
-          template
-        </p>
-        <p></p>
-        <p></p>
-      </div>
-
-      <div class="container">
-        <div class="container">
-          <div class="row">
-            <p> Links to the other pages based on Rank</p>
-            {rank
-              ? Array(rank)
-                  .fill(1)
-                  .map((el, i) => {
-                    return (
-                      <div key={i} class="col-sm">
-                        <a href="/">{UserRank[i + 1]}</a>
-                      </div>
-                    );
-                  })
-              : ""}
-          </div>
-        </div>
-      </div>
-
-      <div class="container">
-        <div class="container">
-          <div class="row">
-            <p> Links </p>
-            <div class="col-sm">
-              <a href="/">Learn More</a>
-            </div>
-            <div class="col-sm">
-              <a href="/">About</a>
-            </div>
-            <div class="col-sm">
-              <a href="/">Contact US</a>
-            </div>
-            <div class="col-sm">
-              <a href="/">Team</a>
-            </div>
-          </div>
-
-          <div class="row">
-            <p> Socials </p>
-            <div class="col-sm">
-              <a href="/">Instagram</a>
-            </div>
-            <div class="col-sm">
-              <a href="/">Telegram</a>
-            </div>
-            <div class="col-sm">
-              <a href="/">Discord</a>
-            </div>
-            <div class="col-sm">
-              <a href="/">Facebook</a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </Fragment>
-  );
+            </div> 
+            */
 }
-
-export default App;
